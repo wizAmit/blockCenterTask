4edbc63bea8189c9b011025b79fdc5864c2addee
Amit Kr. Chandra <amitforfriends_dns@yahoo.com>
2023-10-20 15:57:50 -0400

    added files

--- react-frontend/src/components/PDFButton.js
+++ react-frontend/src/components/PDFButton.js
@@ -2,6 +2,24 @@
 import React from 'react';
 
 const PDFButton = ({ onClick }) => {
+  const downloadPDF = async () => {
+    try {
+      const pdfUrl = 'http://127.0.0.1:5000/get-pdf/sample';
+
+      // Fetch the PDF content from the URL
+      const response = await fetch(pdfUrl);
+      const pdfData = await response.arrayBuffer();
+
+      // Convert the data to a Blob
+      const pdfBlob = blobUtil.arrayBufferToBlob(pdfData, 'application/pdf');
+
+      // Save the Blob as a file with the desired file name
+      saveAs(pdfBlob, 'downloaded.pdf');
+    } catch (error) {
+      console.error('Error downloading PDF:', error);
+    }
+  };
+
   return (
     <div>
       <button onClick={onClick}>Get PDF</button>
